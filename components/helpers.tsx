57ebf8c089057d9c2996bf9044ac0f1067032d96
Adrian <af@Adrians-MacBook-Air.local>
2026-04-30 12:41:13 +0200

    fix links in stories

--- components/helpers.tsx
+++ components/helpers.tsx
@@ -2,23 +2,24 @@ import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
 export function LinkWrapper(props: { link: string; content: React.ReactNode, underlined?: boolean }) {
-  const isExternalLink = props.link.startsWith("http");
+  const normalizedLink = props.link.replace(/^https?:\/(?!\/)/, (match) => `${match}/`);
+  const isExternalLink = /^https?:\/\//.test(normalizedLink);
 
-  if (!props.link) {
+  if (!normalizedLink) {
     return props.content;
   }
 
   return (
     <Link
       className="no-line-height"
-      href={props.link}
+      href={normalizedLink}
       target={isExternalLink ? "_blank" : undefined}
       rel={isExternalLink ? "noopener noreferrer" : undefined}
       itemProp="name"
       title={
         isExternalLink
-          ? `External link to ${props.link}`
-          : `Link to ${props.link}`
+          ? `External link to ${normalizedLink}`
+          : `Link to ${normalizedLink}`
       }
     >
       {props.content}
